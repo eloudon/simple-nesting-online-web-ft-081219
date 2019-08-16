69b284e457780bab92417a655f46a9ba4c9bdc09
Eric Loudon <loudon.eric@gmail.com>
2019-08-16 11:23:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash
 
 end
 
@@ -105,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
 
